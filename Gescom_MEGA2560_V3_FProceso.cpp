0a8994531174c3843beb1e5893fe359fdceee73b
Antonio Arnaiz Lago <antonio.arnaizlago@gmail.com>
2017-04-06 10:55:08 +0200

    Actualizacion tablas en BBDD

--- Gescom_MEGA2560_V3_FProceso.cpp
+++ Gescom_MEGA2560_V3_FProceso.cpp
@@ -1,4 +1,3 @@
-
 // --------------------------------------------------------------------
 //
 //                       GESCOM
@@ -20,10 +19,11 @@
 // -------------------------------------------------------------------
 
 
+
+// -------------------------------------------------------------------
+// Fecha Generación: 06/04/2017 , 10:28
 // -------------------------------------------------------------------
 
-// Fecha Generación: 20/04/2016 , 12:06
-// -------------------------------------------------------------------
 
 
 #include <stdio.h>
@@ -33,7 +33,6 @@
 #include <Arduino.h>
 #include "Gescom_MEGA2560_V3.h"
 #include "Gescom_MEGA2560_V3_CMD.h"
-
 
 
 
@@ -64,12 +63,35 @@ int GESCOM3::procesaComando_02(void)
 
 
 
-case ( IDE_CMD_CM_R00): {  cmd_Comando_CM_R00(&gd);  break; }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                case ( IDE_CMD_CM_R00): {  cmd_Comando_CM_R00(&gd);  break; }
                 case ( IDE_CMD_CM_R01): {  cmd_Comando_CM_R01(&gd);  break; }
 
                 case ( IDE_CMD_CM_DBG): {  cmd_Comando_CM_DBG(&gd);  break; }
+
                 case ( IDE_CMD_B_LIPO): {  cmd_Comando_B_LIPO(&gd);  break; }
 
                 case ( IDE_CMD_B_PPAK): {  cmd_Comando_B_PPAK(&gd);  break; }
@@ -83,11 +105,24 @@ case ( IDE_CMD_CM_R00): {  cmd_Comando_CM_R00(&gd);  break; }
                 case ( IDE_CMD_R_TIME): {  cmd_Comando_R_TIME(&gd);  break; }
 
                 case ( IDE_CMD_L_LEDS): {  cmd_Comando_L_LEDS(&gd);  break; }
-
                 case ( IDE_CMD_C_MIZQ): {  cmd_Comando_C_MIZQ(&gd);  break; }
 
                 case ( IDE_CMD_C_MDER): {  cmd_Comando_C_MDER(&gd);  break; }
+
                 case ( IDE_CMD_C_RMOV): {  cmd_Comando_C_RMOV(&gd);  break; }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
          }
